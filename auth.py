bf39afb2ef36dff8a9a0b5b77a00950d0584c5a1
Naramala Siva Vekata Pavan <sivavenkatapavan@gmail.com>
2025-03-17 08:37:59 +0530

    feat: Add auth.py to authenticate to spotify

---
+++ auth.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Load environment variables
+load_dotenv()
+
+# Access variables
+SPOTIPY_CLIENT_ID = os.getenv("SPOTIPY_CLIENT_ID")
+SPOTIPY_CLIENT_SECRET = os.getenv("SPOTIPY_CLIENT_SECRET")
+SPOTIPY_REDIRECT_URI = os.getenv("SPOTIPY_REDIRECT_URI")
+
+# Authenticate with Spotify API
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=SPOTIPY_CLIENT_ID,
+    client_secret=SPOTIPY_CLIENT_SECRET,
+    redirect_uri=SPOTIPY_REDIRECT_URI,
+    scope="playlist-read-private"
+))
+
+print("Authenticated successfully!")
