9c29aec7332e505a734fe2f6a92bacbd25db52e7
Naramala Siva Vekata Pavan <sivavenkatapavan@gmail.com>
2025-03-17 08:49:08 +0530

    Modify fetch.py to fetch playlists

--- fetch.py
+++ fetch.py
@@ -1,19 +1,11 @@
 from auth import authenticate_spotify
 
-def get_playlist_tracks(playlist_id):
+def get_user_playlists():
     sp = authenticate_spotify()
-    results = sp.playlist_tracks(playlist_id)
-    tracks = []
+    playlists = {}
 
-    for item in results['items']:
-        track = item['track']
-        song_name = f"{track['name']} {track['artists'][0]['name']}"
-        tracks.append(song_name)
+    results = sp.current_user_playlists()
+    for playlist in results['items']:
+        playlists[playlist['id']] = playlist['name']
 
-    return tracks
-
-# Example usage
-if __name__ == "__main__":
-    playlist_id = "YOUR_PLAYLIST_ID"
-    songs = get_playlist_tracks(playlist_id)
-    print("Songs found:", songs)
+    return playlists
